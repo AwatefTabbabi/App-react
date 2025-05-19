9ec65060fd75bdc2b26f9889207178fbb572642e
Awatef <tabbabiawatef@gmail.com>
2025-05-19 16:46:59 +0100

    mis a jour

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext,props } from "../context/AuthContext";
 import axios from "axios";
 
 const Login = ({ onLogin }) => {
